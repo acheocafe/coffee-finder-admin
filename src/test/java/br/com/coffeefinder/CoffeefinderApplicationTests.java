46523a5b0bc5fbd976e61f05ed2cae95a394b4cb
Nathan Martins <nathanmartins@outlook.com>
2022-06-29 18:10:06 -0300

    add base spring app

---
+++ src/test/java/br/com/coffeefinder/CoffeefinderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coffeefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeefinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
