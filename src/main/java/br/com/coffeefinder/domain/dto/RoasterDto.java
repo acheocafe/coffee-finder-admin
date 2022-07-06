06bf746b57503eba68c0c49838d7c245ee78f5fc
Nathan Martins <nathanmartins@outlook.com>
2022-07-06 16:47:29 -0300

    add test for notfound and refactor some classes

--- src/main/java/br/com/coffeefinder/domain/dto/RoasterDto.java
+++ src/main/java/br/com/coffeefinder/domain/dto/RoasterDto.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class RoasterDto {
 
-  private Long id;
+  private String id;
   private String name;
   private String email;
   private String phone;
