12f269216b27d619afca743c91b69e149bd039b1
Nathan Martins <nathanmartins@outlook.com>
2022-07-08 18:38:22 -0300

    add test cases
    add  find all and save roaster success tests

--- src/test/java/br/com/coffeefinder/mockhelpers/service/RoasterServiceHelper.java
+++ src/test/java/br/com/coffeefinder/mockhelpers/service/RoasterServiceHelper.java
@@ -2,6 +2,7 @@ package br.com.coffeefinder.mockhelpers.service;
 
 import br.com.coffeefinder.domain.dto.RoasterDto;
 import br.com.coffeefinder.domain.model.Roaster;
+import java.util.List;
 import java.util.Optional;
 
 public class RoasterServiceHelper {
@@ -12,7 +13,7 @@ public class RoasterServiceHelper {
             .id(2L)
             .name("Roaster2")
             .email("roaster2@outlook.com")
-            .phone("21981582550")
+            .phone("552299922244")
             .build());
   }
 
@@ -21,7 +22,59 @@ public class RoasterServiceHelper {
         .id("2")
         .name("Roaster2")
         .email("roaster2@outlook.com")
-        .phone("21981582550")
+        .phone("552299922244")
+        .build();
+  }
+
+  public static List<RoasterDto> mockExpectedFindAll() {
+
+    return List.of(
+        RoasterDto.builder()
+            .id("2")
+            .name("Roaster2")
+            .email("roaster2@outlook.com")
+            .phone("552299922242")
+            .build(),
+        RoasterDto.builder()
+            .id("3")
+            .name("Roaster3")
+            .email("roaster3@outlook.com")
+            .phone("552299922243")
+            .build());
+  }
+
+  public static List<Roaster> mockFindAllRespository() {
+
+    return List.of(
+        Roaster.builder()
+            .id(2L)
+            .name("Roaster2")
+            .email("roaster2@outlook.com")
+            .phone("552299922242")
+            .build(),
+        Roaster.builder()
+            .id(3L)
+            .name("Roaster3")
+            .email("roaster3@outlook.com")
+            .phone("552299922243")
+            .build());
+  }
+
+  public static RoasterDto mockExpectedSaveRoaster() {
+    return RoasterDto.builder()
+        .id("1")
+        .name("Roaster2")
+        .email("roaster2@outlook.com")
+        .phone("552299922244")
+        .build();
+  }
+
+  public static Roaster mockSaveRoasterRepository() {
+    return Roaster.builder()
+        .id(2L)
+        .name("Roaster2")
+        .email("roaster2@outlook.com")
+        .phone("552299922244")
         .build();
   }
 }
--- src/test/java/br/com/coffeefinder/service/RoasterServiceTest.java
+++ src/test/java/br/com/coffeefinder/service/RoasterServiceTest.java
@@ -3,9 +3,13 @@ package br.com.coffeefinder.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import br.com.coffeefinder.domain.mapper.RoasterMapper;
+import br.com.coffeefinder.domain.model.Roaster;
 import br.com.coffeefinder.exception.RoasterNotFoundException;
 import br.com.coffeefinder.mockhelpers.service.RoasterServiceHelper;
 import br.com.coffeefinder.repository.RoasterRepository;
@@ -36,6 +40,33 @@ class RoasterServiceTest {
     var actual = roasterService.findById("2");
     assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
   }
+
+  @Test
+  void test_find_all_success() {
+
+    var expected = RoasterServiceHelper.mockExpectedFindAll();
+
+    when(roasterRepository.findAll()).thenReturn(RoasterServiceHelper.mockFindAllRespository());
+    var actual = roasterService.findAll();
+    assertThat(actual).usingRecursiveFieldByFieldElementComparator().isEqualTo(expected);
+  }
+
+  @Test
+  void test_save_roaster_success() {
+
+    when(roasterRepository.save(any(Roaster.class)))
+        .thenReturn(RoasterServiceHelper.mockSaveRoasterRepository());
+    roasterService.save(RoasterServiceHelper.mockExpectedSaveRoaster());
+    verify(roasterRepository, times(1)).save(any());
+  }
+
+  @Test
+  // TODO: implement
+  void test_update_roaster_success() {}
+
+  @Test
+  // TODO: implement
+  void test_delete_roaster_success() {}
 
   @Test
   void test_nonexistent_roaster_id_should_throw_exception() {
